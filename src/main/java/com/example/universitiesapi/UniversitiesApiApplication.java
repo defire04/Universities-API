d872679747393d7cd1d5760714922edacbde9325
Dima Olaba <defire04st@gmail.com>
2023-03-17 20:29:03 +0200

    Initial commit

---
+++ src/main/java/com/example/universitiesapi/UniversitiesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.universitiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversitiesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversitiesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/universitiesapi/UniversitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.universitiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitiesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
